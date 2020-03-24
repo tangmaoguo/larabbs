f607cbe6ff9f9d0ee2dd9be113817d44b1005745
vagrant <vagrant@homestead>
2020-03-24 06:59:14 +0000

    编辑个人资料

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'regex:/^[a-zA-Z0-9\-\_]+$/','unique:users,name,'.Auth::id(),'between:3,20'],
+            'email' => ['required', 'email', 'max:255','unique:users,email,'.Auth::id()],
+            'introduction'=>['max:200']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '用户名不能为空',
+            'name.regex' => '用户名只支持数字、英文、下划线和横杠',
+            'name.unique' => '用户名已被占用',
+            'name.between' =>'用户名必须介于 3 - 20 个字符之间'
+        ];
+    }
+}
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">
+                    <h4> <i class="glyphicon glyphicon-edit"></i> 编辑个人资料</h4>
+                </div>
+
+                <div class="card-body">
+                    <form action="{{ route('users.update',$user->id) }}" method="post">
+                        @method('put')
+                        @csrf
+                        @include('shared._error')
+                        <div class="form-group">
+                            <label for="name">用户名</label>
+                            <input type="text" class="form-control" id="name" name="name" value="{{ old('name',$user->name) }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">邮 箱</label>
+                            <input class="form-control" type="email" name="email" id="email" value="{{ old('email', $user->email) }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="introduction">个人简介</label>
+                            <textarea name="introduction" id="introduction" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+                        </div>
+                        <div class="well well-sm">
+                            <button type="submit" class="btn btn-primary">保存</button>
+                        </div>
+
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
