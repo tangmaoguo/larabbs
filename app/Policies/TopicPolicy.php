a49c0b217a4d2cb9529e0b8c14f86b678e4e99f7
vagrant <vagrant@homestead>
2020-04-06 07:37:53 +0000

    删除话题“

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,13 +7,18 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
+    protected function isAuthorOf($user,$topic)
+    {
+        return $user->id == $topic->user_id;
+    }
+
     public function update(User $user, Topic $topic)
     {
-        return $topic->user_id == $user->id;
+        return $this->isAuthorOf($user,$topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        return $this->isAuthorOf($user,$topic);
     }
 }
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -36,16 +36,23 @@
                     <div class="topic-body mt-4 mb-4">
                         {!! $topic->body !!}
                     </div>
+                    @can('update', $topic)
+                        <div class="operate">
+                            <hr>
+                            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                                <i class="far fa-edit"></i> 编辑
+                            </a>
+                            <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
+                                  style="display: inline-block;">
+                               @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                                    <i class="far fa-trash-alt"></i> 删除
+                                </button>
+                            </form>
+                        </div>
+                    @endcan
 
-                    <div class="operate">
-                        <hr>
-                        <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-                            <i class="far fa-edit"></i> 编辑
-                        </a>
-                        <a href="" class="btn btn-outline-secondary btn-sm" role="button">
-                            <i class="far fa-trash-alt"></i> 删除
-                        </a>
-                    </div>
                 </div>
             </div>
         </div>
